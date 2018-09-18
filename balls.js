c8d50d494906d447c98b3c82e13e765c1827f727
Explorer <vitaljan@gmail.com>
2018-09-18 16:10:43 +0300

    2018-0918-2

--- balls.js
+++ balls.js
@@ -1,25 +1,28 @@
-var balls = [];
 
 var $_ = (e) => {
     return document.getElementById(e);
 }
 
-var g = 1;
-var ground = 500;
-var fps = 30;
-
 var rnd = (a) => {
     return Math.round(Math.random()*a);
 }
 
+//глобальные пер-ные
+var g = 1; // уск. св. падения
+var ground = 500; // уровень земли в px
+var fps = 30; // частота кадров
+
+
+var balls = []; // масс. объектов
+
 class Ball {
     constructor (ID, Start) {
         this.id = ID;
-        this.start = Start;
-        this.dy = Start;
-        this.y = ground;
-        this.sqTime = 0;
-        this.sqScale = 1;
+        this.start = Start; // ускорение на земле
+        this.dy = Start; // дельта y
+        this.y = ground; // тек. y
+        this.sqTime = 0; // время в сжатом виде
+        this.sqScale = 1; // коеф. сжатия
     }
     move () {
         this.y -= this.dy;
@@ -42,13 +45,13 @@ class Ball {
 }
 
 function reset() {
-for (var x=0; x<10; x++) {
+for (var x=0; x<10; x++) { // визуализация и инициализация объектов
         $_('body').innerHTML += `<div class='ball' id='ball${x}'></div>`;
         $_(`ball${x}`).style.backgroundColor = `rgb(${x*20},${250-x*20},${rnd(155)})`;
         balls.push(new Ball(`ball${x}`, rnd(20)+10));
     console.log(balls[x]);
     }
-var interval = setInterval(function(){
+var interval = setInterval(function(){ // перемещение объектов
     for (x=0; x<10; x++) {
         balls[x].move();
     }
